0afa956afc998b74e5aaf023d5f4cd681cd9bfba
azad <email.asaad@mutareb.com>
2023-04-01 15:12:50 +0000

    added pandas route and requirement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -15,6 +16,19 @@ def echo(name):
     val = {"new-name": name}
     return jsonify(val)
 
+@app.route('/html')
+def html():
+    """Returns some custome HTML"""
+    return """
+    <title>This is a Hello World Page</title>
+    <p>Hello</p>
+    <p><b>World</b></p>
+    """
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
